17efd98f0b1d97785cf5cf23d3fe92a91e476b05
Abbas <kareemabbas106@gmail.com>
2021-11-10 01:11:28 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Laravel REST API with Sanctum
+
+This is an example of a REST API using auth tokens with Laravel Sanctum
+
+#Using SQLite
+
+## Routes
+
+```
+# Public
+
+GET   /api/products
+GET   /api/products/:id
+
+POST   /api/login
+@body: email, password
+
+POST   /api/register
+@body: name, email, password, password_confirmation
+
+
+# Protected
+
+POST   /api/products
+@body: name, slug, description, price
+
+PUT   /api/products/:id
+@body: ?name, ?slug, ?description, ?price
+
+DELETE  /api/products/:id
+
+POST    /api/logout
+```
---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
